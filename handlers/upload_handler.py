1e847a1b069d329df4284022b39c872b36c93d52
Sunpy <sunpy@osufx.com>
2018-01-23 13:10:29 +0100

    Check if file was passed in upload request

--- handlers/upload_handler.py
+++ handlers/upload_handler.py
@@ -2,11 +2,12 @@ import glob
 
 def upload(request):
     #Check if the params is declared
-    if any(x not in request.args for x in ["user", "password"]):
+    if any(x not in request.args for x in ["user", "password"]) or "file" not in request.files:
         return "Missing params"
 
     u = request.args["user"]
     p = request.args["password"]
+    f = request.files['file']
 
     #Check if valid login
     if u not in glob.users or glob.users[u] != p:
